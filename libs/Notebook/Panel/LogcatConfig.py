9e1ee74eda6382304c9dd99766124be2fbe10def
wuyou1102 <jotey@qq.com>
2018-10-18 10:11:33 +0800

    add

--- libs/Notebook/Panel/LogcatConfig.py
+++ libs/Notebook/Panel/LogcatConfig.py
@@ -9,8 +9,6 @@ class LogcatConfig(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize,
                           style=wx.TAB_TRAVERSAL)
-        self.SetBackgroundColour("red")
-
         MainSizer = wx.BoxSizer(wx.HORIZONTAL)
         config_sizer = self.__init_logcat_config_sizer()
         MainSizer.Add(config_sizer, 1, wx.EXPAND | wx.ALL, 1)
@@ -30,8 +28,23 @@ class LogcatConfig(wx.Panel):
         sizer = wx.BoxSizer(wx.VERTICAL)
         wx_checkbox = wx.CheckBox(self, wx.ID_ANY, u"过滤器")
         wx_checkbox.Bind(wx.EVT_CHECKBOX, self.on_check)
+        sizer.Add(wx_checkbox, 0, wx.ALL, 0)
 
         return sizer, wx_checkbox
+
+    def __init_priority_sizer(self):
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        text = wx.StaticText(self, wx.ID_ANY, u":")
+        tag = wx.TextCtrl(self, wx.ID_ANY)
+        w, h = tag.GetSize()
+        plus = wx.Button(self, wx.ID_ANY, u"+", size=(40, h))
+        choice_ctrl = wx.Choice(self, wx.ID_ANY,
+                                choices=[u"Verbose", u"Debug", u"Info", u"Warning", u"Error", u"Fatal", u"Silent"])
+        sizer.Add(tag, 1, wx.ALL, 1)
+        sizer.Add(text, 0, wx.LEFT | wx.RIGHT, 5)
+        sizer.Add(choice_ctrl, 0, wx.ALL, 1)
+        sizer.Add(plus, 0, wx.ALL, 1)
+        return sizer
 
     def __init_logcat_config_sizer(self):
         sizer = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"日志配置"), wx.VERTICAL)
@@ -41,9 +54,12 @@ class LogcatConfig(wx.Panel):
                                                               choices=[u"Default", u"Brief", u"Process", u"Tag",
                                                                        u"Thread", u"Raw", u"Time", u"Long"])
         filter_sizer, self.filter_checkbox = self.__init_filter()
+        priority_sizer = self.__init_priority_sizer()
 
         sizer.Add(buff_sizer, 0, wx.EXPAND | wx.ALL, 2)
         sizer.Add(format_sizer, 0, wx.EXPAND | wx.ALL, 2)
+        sizer.Add(filter_sizer, 0, wx.EXPAND | wx.ALL, 2)
+        sizer.Add(priority_sizer, 0, wx.EXPAND | wx.ALL, 2)
         return sizer
 
     def on_check(self, event):
